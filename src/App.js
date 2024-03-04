37e02091af98a189c5533a20974fead118819a1e
fagoon1311 <fs14smailbox65@gmail.com>
2024-03-04 22:23:30 +0530

    routes setup

--- src/App.js
+++ src/App.js
@@ -1,17 +1,32 @@
 import './App.css';
-import About from './components/About';
-import Cover from './components/Cover';
-import Header from './components/Header';
-import Services from './components/Services';
+import Discover from './components/Discover';
+// import About from './components/About';
+// import Cover from './components/Cover';
+// import Header from './components/Header';
+import Layout from './components/Layout';
+import MainPage from './components/MainPage';
+// import Services from './components/Services';
+import {RouterProvider, createBrowserRouter} from 'react-router-dom'
+
+const appRouter = createBrowserRouter([{
+  path:"/",
+  element: <Layout />,
+  children:[
+    {
+      path:"/",
+      element:<MainPage />
+    },
+    {
+      path:"/discover",
+      element: <Discover />    
+    }
+  ]
+}])
 
 function App() {
   return (
     <div className="">
-      <Header />
-      <Cover />
-      <About />
-      <Services />
-      
+      <RouterProvider router={appRouter}/>
     </div>
   );
 }
---
+++ src/components/MainPage.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import Cover from '../components/Cover';
+import Services from '../components/Services';
+import About from '../components/About';
+
+
+const MainPage = () => {
+  return (
+    <div>
+      <Cover />
+      <About />
+      <Services />
+    </div>
+  )
+}
+
+export default MainPage
