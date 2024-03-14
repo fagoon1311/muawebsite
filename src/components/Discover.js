b943d03f74ef318270d01607c1e1c40730476aae
fagoon1311 <fs14smailbox65@gmail.com>
2024-03-14 23:07:50 +0530

    .

--- src/components/Discover.js
+++ src/components/Discover.js
@@ -1,15 +1,16 @@
 import React from 'react'
 import discover1 from '../assets/images/discover1.jpg'
 import FAQ from './FAQ.js'
+import faqbg from '../assets/images/faqbg.jpg'
 const Discover = () => {
   return (
     <div>
         <div className='flex flex-col items-center mt-24   '>
             <h1 className='font-Poppins text-pink-500'>Our Services</h1>
-            <h1 className='font-Poppins text-5xl font-bold py-6'>Face Makeup</h1>
+            <h1 className='font-Poppins text-5xl font-bold py-4'>Face Makeup</h1>
         </div>
         <div className='flex flex-col items-center mt-4 '>
-            <img src={discover1} className='h-[45rem] w-[70rem] rounded-md shadow-lg'></img>
+            <img src={discover1} className='h-[45rem] w-[70rem] rounded-md shadow-lg mt-10'></img>
             <div className='w-[80rem] mt-4'>
               <p className='font-Poppins'>Face makeup plays a crucial role in creating a polished and perfected appearance, 
               enhancing natural beauty while allowing for creativity and expression. 
@@ -22,8 +23,14 @@ const Discover = () => {
             </div>
 
         </div>
-        <div>
-          <FAQ />
+        <div className='mt- mt-24'>
+          <div className='flex flex-col items-center'>
+            <h1 className='font-Poppins text-pink-500'>Frequently asked questions</h1>
+            <h1 className='font-Poppins text-6xl p-3 font-semibold'>Popular Questions</h1>
+          </div>
+          <div style={{backgroundImage:`url(${faqbg})`, backgroundSize:'100vw'}}>
+            <FAQ />
+          </div>
         </div>
     </div>
   )
--- src/components/FAQ.js
+++ src/components/FAQ.js
@@ -5,17 +5,16 @@ import { ChevronDown, ChevronUp } from 'lucide-react';
 
 const FaqCard = ({title, answer, isVisible, setIsVisible}) =>{
     return (
-        <div className='bg-slate-100 m-2 p-2 w-[50rem]'>
-            <span className='font-Poppins'>{title}</span>
-            <span>
-              {
-                isVisible&&<p className='font-Poppins'>{answer}</p>
-              }
-            </span>
+        <div className={` m-2 p-2 w-[50rem] flex justify-between rounded-xl ${isVisible?"bg-pink-50":" bg-slate-100"}`}> 
+            <div className='flex flex-col'>
+            <span className={`font-Poppins text-xl ${isVisible ? "font-semibold" : ""}`}>{title}</span>
+            <span>{isVisible&&<p className='font-Poppins text-sm'>{answer}</p>}</span>
+            </div>
+            <div>
             <button onClick={setIsVisible}>
                 {isVisible ? <ChevronUp color="#ff0088" /> : <ChevronDown color="#ff0088" />}
             </button>
-            
+            </div>
         </div>
     )
 }
